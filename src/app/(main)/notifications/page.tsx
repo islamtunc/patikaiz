1847bf52dbb6652bda7fd569410c46ffb377b5c7
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-16 01:36:50 +0300

    Update page.tsx

--- src/app/(main)/notifications/page.tsx
+++ src/app/(main)/notifications/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="text-center text-2xl font-bold">Haya te heye?</h1>
+          <h1 className="text-center text-2xl font-bold">Haya te pê heye?</h1>
         </div>
         <Notifications />
       </div>
