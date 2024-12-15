1f91b60cde2dbfe88a2aa17c7cc2be1ee1d03fff
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-15 22:23:20 +0300

    Update stream.ts

--- src/lib/stream.ts
+++ src/lib/stream.ts
@@ -5,7 +5,7 @@
 import { StreamChat } from "stream-chat";
 
 const streamServerClient = StreamChat.getInstance(
-  process.env.NEXT_STREAM!,
+  process.env.NEXT_PUBLIC_STREAM_KEY!,
   process.env.STREAM_SECRET,
 );
 
