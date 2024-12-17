e7d17938ddda733b14be800c30774070de1ed332
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-18 02:53:55 +0300

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <h5 className="text-center text-3xl font-bold"> Ser nameya namê ,namê Allah<br></<br>
- Bê nameye namê kêmî wî Wallah"</h5>
+          <h5 className="text-center text-3xl font-bold"> Ser nameya namê ,namê Allah<span></span>
+ Bê nameye namê kêmî wî Wallah</h5>
          
           <h1 className="text-center text-3xl font-bold">Lebê ?</h1>
 
