5530c71398e6276b43193630bd96b4ec81f62366
cigerxwinislam <islamtunc748@gmail.com>
2025-01-17 11:58:24 -0500

    serxere nu

--- src/app/(main)/mmavahi/posts/[postId]/page.tsx
+++ src/app/(main)/mmavahi/posts/[postId]/page.tsx
@@ -20,7 +20,7 @@ interface PageProps {
 }
 
 const getPost = cache(async (postId: string, loggedInUserId: string) => {
-  const post = await prisma.mmkedkar.findUnique({
+  const post = await prisma.mmavahi.findUnique({
     where: {
       id: postId,
     },
