aa4bbd66d73adac2ae8c31dbfe2982e057e39498
cigerxwinislam <islamtunc748@gmail.com>
2025-01-18 09:07:31 -0500

    serxere nu

--- src/components/mmhewcedari/editor/mm/mmkr.tsx
+++ src/components/mmhewcedari/editor/mm/mmkr.tsx
@@ -14,9 +14,9 @@ import { useDropzone } from "@uploadthing/react";
 import { ImageIcon, Loader2, X } from "lucide-react";
 import Image from "next/image";
 import { ClipboardEvent, useRef, useState } from "react";
-import { useSubmitPostMutation } from "./mutations";
+import { useSubmitPostMutation } from "../mutations";
 import "./styles.css";
-import useMediaUpload, { Attachment } from "./useMediaUpload";
+import useMediaUpload, { Attachment } from "../useMediaUpload";
 import { Input } from "@/components/ui/input";
 
 export default function MmPostEditor() {
