6f543069bb10328d1fdcbe9971c947d50c5540aa
cigerxwinislam <islamtunc748@gmail.com>
2025-05-29 21:12:19 +0300

    serxere nu

--- mela/src/app/(main)/layout.tsx
+++ mela/src/app/(main)/layout.tsx
@@ -9,6 +9,10 @@ import Navbar from "./Navbar";
 import SessionProvider from "./SessionProvider";
 
 import 'bootstrap/dist/css/bootstrap.css'
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import "stream-chat-react/dist/css/v2/index.css";
+import useInitializeChatClient from "./messages/useInitializeChatClient";
 export default async function Layout({
   children,
 }: {
@@ -17,6 +21,7 @@ export default async function Layout({
   const session = await validateRequest();
 
   if (!session.user) redirect("/malper");
+  const chatClient = useInitializeChatClient();
 
   return (
     <SessionProvider value={session}>
@@ -24,7 +29,11 @@ export default async function Layout({
         <Navbar />
         <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
           <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
-          {children}
+          {chatClient && (
+            <Chat client={chatClient} theme="str-chat__theme-light">
+              {children}
+            </Chat>
+          )}
         </div>
         <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
