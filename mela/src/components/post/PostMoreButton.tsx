cadedd3ae4a7e6506e15758e3f8bc2569554a0ac
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-07-28 17:32:20 +0300

    Create PostMoreButton.tsx

---
+++ mela/src/components/post/PostMoreButton.tsx
@@ -0,0 +1,56 @@
+// Bismillahirrahmanirrahim
+// Elhamdülillahirabbulalemin 
+// Esselatu vesselamu ala rasulina Muhammed'in ve ala alihi ve sahbihi, ecmain 
+// SuphanAllah ul Azim ve bihamdihi 
+// Allah u Ekber 
+// La ilahe illallah u vahdehu la şerike leh ,lehul mulku ve lehul hamdu
+
+
+import { PostData } from "@/lib/types";
+import { MoreHorizontal, Trash2 } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import DeletePostDialog from "./DeletePostDialog";
+
+interface PostMoreButtonProps {
+  post: PostData;
+  className?: string;
+}
+
+export default function PostMoreButton({
+  post,
+  className,
+}: PostMoreButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2 className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostDialog
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+}
