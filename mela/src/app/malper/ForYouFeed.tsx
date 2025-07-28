12a05a3a9b8454f6ef10ba4ef553911b9fec43de
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-07-28 19:56:28 +0300

    Elhamdülillahirabbulalemin

--- mela/src/app/malper/ForYouFeed.tsx
+++ mela/src/app/malper/ForYouFeed.tsx
@@ -6,8 +6,8 @@
 "use client";
 
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
-import Post from "@/components/mmavahi/Post";
-import PostsLoadingSkeleton from "@/components/mmavahi/PostsLoadingSkeleton";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 import kyInstance from "@/lib/ky";
 import { PostsPage } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
