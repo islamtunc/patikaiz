d6c1744cde99241f9b195d9ceebd0797248aaccc
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-08-07 14:49:01 +0300

    SuphanAllah be Bihamdihi

---
+++ mela/src/app/(main)/posts/layout.tsx
@@ -0,0 +1,7 @@
+//Bismillahirrahmanirahim 
+//Elhamdulillahirabbulalemin 
+// Es selatu vesselamu ala rasulina Muhammed'in ve ala alihi ve sahbihi ecmain.
+//SuphanAllah ul Azim ve Bihamdihi
+//Allah U Ekber velillahilhamd
+//La ilahe illAllah u vahdehu la şerike leh,lehul mulku ve lehul hamdu
+//Ve huwe ala kulli şey in kadir
