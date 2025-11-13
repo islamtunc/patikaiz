da604de73baef1ab752d8eb904087ef82b31fae5
mmrxistin <mmrxistin@gmail.com>
2025-11-13 21:22:45 +0300

    suphanAllahî ve Bîhamdîhi velhamdulillah

--- mela/src/app/(main)/Navbar.tsx
+++ mela/src/app/(main)/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
-          Lebê 
+          Patika
         </Link>
         <SearchField />
         <UserButton className="sm:ms-auto" />
