3b00b9000315fa748cb2d21cf36f967f7a0582b4
mmrxistin <mmrxistin@gmail.com>
2025-11-13 20:00:35 +0300

    SuphanAllahî ve Bihamdihi velhamdulîllah

--- mela/src/app/layout.tsx
+++ mela/src/app/layout.tsx
@@ -1,4 +1,16 @@
 // Bismillahirrahmanirrahim 
+// Elhamdulillahi Rabbil Alamin
+// Essalatu vesselamu ala Resulina Muhammedin
+// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
+// Subhanallah, Elhamdulillah, Allahu Ekber
+// La ilahe illallah, Muhammedur Resulullah
+// La havle vela kuvvete illa billah
+// Astagfirullah al azim
+// La ilahe illallah, wahdahu la sharika lahu, lahul mulku wa lahul hamdu yuhyi wa yumit wa huwa ala kulli shay'in qadir
+// Seyyidena ve nebiyyena Muhammedun abduhu ve rasuluhu
+// Subhanallahi wa bihamdihi, subhanallahil azim
+// ELHAMDULILLAHI RABBIL 'ALAMIN
+// Allah U Ekber ve lillahi'l-hamd
 
 
 
