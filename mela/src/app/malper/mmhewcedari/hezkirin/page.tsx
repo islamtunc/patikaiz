13b6bc649405f3ac8a8795198b1ab67d025290ca
mmrxistin <mmrxistin@gmail.com>
2025-12-01 20:21:07 +0300

    la ilahe illAllah

---
+++ mela/src/app/malper/mmhewcedari/hezkirin/page.tsx
@@ -0,0 +1,23 @@
+// Bismillahirrahmanirahim
+// Elhamdulillahirabbulalemin
+// Ve salatu ve selamu ala resulina Muhammedin ve alihi ve sahbihi ecmain
+// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
+// Subhanallah, Elhamdulillah, Allahu Ekber
+// Estağfirullah El-Azim
+
+"use client";
+import React from "react";
+import { Card } from "react-bootstrap";
+function page() {
+  return (
+    <div>
+    <Card style={{ marginTop: "20px", width: "100%", maxWidth: "700px", textAlign: "center", padding: "19px" }}>
+        <Card.Title>manzara</Card.Title>
+        <Card.Body>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default page
