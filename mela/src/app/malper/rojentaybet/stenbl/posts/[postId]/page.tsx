adba6b7469d756ef25b1a176834579237b8169b5
mmrxistin <mmrxistin@gmail.com>
2025-12-12 15:03:55 +0300

    Subhanallah, Elhamdillah, Allahu Ekber

--- mela/src/app/malper/rojentaybet/stenbl/posts/[postId]/page.tsx
+++ mela/src/app/malper/rojentaybet/stenbl/posts/[postId]/page.tsx
@@ -2,19 +2,12 @@
 // Elhamdulillahi Rabbil Alamin
 // Essalatu vesselamu ala Resulina Muhammedin 
 // Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
-// Subhanallah, Elhamdulillah, Allahu Ekber
-import Linkify from "@/hemanen/Linkify";
+// Subhanallah, Elhamdillah, Allahu Ekber
 import MmmPost from "@/hemanen/diwar/mmPost";
-import Post from "@/hemanen/diwar/mmPost";
-import UserAvatar from "@/hemanen/UserAvatar";
-import UserTooltip from "@/hemanen/UserTooltip";
 import prisma from "@/pirtukxane/prisma";
-import { getStenbolDataInclude, UserData } from "@/pirtukxane/types";
-import { Loader2 } from "lucide-react";
-import { Metadata } from "next";
-import Link from "next/link";
+import { getStenbolDataInclude } from "@/pirtukxane/types";
 import { notFound } from "next/navigation";
-import { cache, Suspense } from "react";
+import { cache } from "react";
 
 interface PageProps {
   params: { postId: string };
@@ -22,10 +15,8 @@ interface PageProps {
 
 const getPost = cache(async (postId: string) => {
   const post = await prisma.stenbol.findUnique({
-    where: {
-      id: postId,
-    },
-    include: getStenbolDataInclude(""), // Kullanıcı doğrulama kaldırıldı
+    where: { id: postId },
+    include: getStenbolDataInclude(""),
   });
 
   if (!post) notFound();
@@ -33,30 +24,21 @@ const getPost = cache(async (postId: string) => {
   return post;
 });
 
-
 export default async function Page({ params: { postId } }: PageProps) {
   const post = await getPost(postId);
 
   return (
-    <main 
-      className="flex flex-col lg:flex-row w-full min-h-screen bg-gradient-to-br from-[#f8fafc] to-[#e2e8f0] p-4"
-    >
-      {/* İçerik Kartı */}
+    <main className="flex flex-col lg:flex-row w-full min-h-screen bg-gradient-to-br from-[#f8fafc] to-[#e2e8f0] p-4">
       <div className="flex-1 flex justify-center items-start">
         <div className="w-full max-w-2xl">
-<MmmPost post={{ ...post, title: post.name }} />
-          {/* Ayraç ve boşluk */}
+          <MmmPost post={{ ...post, title: post.name }} />
           <div className="my-8">
-            <hr className="border-t-4 border-gray-355" />
+            <hr className="border-t-4 border-gray-300" />
           </div>
         </div>
       </div>
     </main>
   );
-}
-
-interface UserInfoSidebarProps {
-  user: UserData;
 }
 
 
