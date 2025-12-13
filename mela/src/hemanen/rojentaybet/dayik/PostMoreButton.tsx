ebd8777b31eda3b4da94f69f37f78814dde788c1
mmrxistin <mmrxistin@gmail.com>
2025-12-13 16:47:34 +0300

    SuphanAllah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/rojentaybet/dayik/PostMoreButton.tsx
+++ mela/src/hemanen/rojentaybet/dayik/PostMoreButton.tsx
@@ -1,11 +1,11 @@
 // Bismillahirrahmanirrahim 
 // Elhamdulillahirabbulalemin
-// Es-selatu vesselamu ala rasulina Muhammedin ve ala alihi ve sahbihi ecmain
+// Es-selatu vesselamu ala rasulina Muhammedin 
 // Allah u Ekber, Allah u Ekber, Allah u Ekber, La ilahe illallah
 // SuphanAllah, Elhamdulillah, Allahu Ekber
 
 
-import { MmavahiData } from "@/pirtukxane/types";
+import { DayikData } from "@/pirtukxane/types";
 import { MoreHorizontal, Trash2 } from "lucide-react";
 import { useState } from "react";
 import { Button } from "../../ui/button";
@@ -18,7 +18,7 @@ import {
 import DeletePostDialog from "./DeletePostDialog";
 
 interface PostMoreButtonProps {
-  post: MmavahiData;
+  post: DayikData;
   className?: string;
 }
 
--- mela/src/hemanen/rojentaybet/hezkirin/PostMoreButton.tsx
+++ mela/src/hemanen/rojentaybet/hezkirin/PostMoreButton.tsx
@@ -1,6 +1,6 @@
 // Bismillahirrahmanirrahim 
 // Elhamdulillahirabbulalemin
-// Es-selatu vesselamu ala rasulina Muhammedin ve ala alihi ve sahbihi ecmain
+// Es-selatu vesselamu ala rasulina Muhammedin 
 // Allah u Ekber, Allah u Ekber, Allah u Ekber, La ilahe illallah
 // SuphanAllah, Elhamdulillah, Allahu Ekber
 
