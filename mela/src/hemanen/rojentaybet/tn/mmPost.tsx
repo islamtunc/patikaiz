9cc166623dc243427b563a0c6e320e0c2519ee2c
mmrxistin <mmrxistin@gmail.com>
2025-12-14 17:19:02 +0300

    SuphanAllah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/rojentaybet/tn/mmPost.tsx
+++ mela/src/hemanen/rojentaybet/tn/mmPost.tsx
@@ -49,8 +49,8 @@ export default function MmmPost({ post }: PostProps) {
         </div>
       </div>
      
-      {!!post.attachments.length && (
-        <MediaPreviews attachments={post.attachments} />
+      {!!attachments.length && (
+        <MediaPreviews attachments={attachments} />
       )}
 
        <Linkify>
@@ -69,8 +69,8 @@ export default function MmmPost({ post }: PostProps) {
         </Button>
         </Card>
 </Linkify>
-   {!!post.attachments.length && (
-        <MediaPreviews attachments={post.attachments} />
+   {!!attachments.length && (
+        <MediaPreviews attachments={attachments} />
       )}
 
       <hr className="text-muted-foreground" />
