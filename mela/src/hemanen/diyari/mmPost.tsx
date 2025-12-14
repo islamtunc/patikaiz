442fa8f0faa2a308108cd349c7551f6b56278042
mmrxistin <mmrxistin@gmail.com>
2025-12-14 17:58:29 +0300

    SuphanAllah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/diyari/mmPost.tsx
+++ mela/src/hemanen/diyari/mmPost.tsx
@@ -22,8 +22,8 @@ interface PostProps {
 }
 
 export default function MmmPost({ post }: PostProps) {
-  const attachments: Media[] = Array.isArray(post.attachments)
-    ? (post.attachments as Media[])
+  const attachments: Media[] = Array.isArray(post.media)
+    ? (post.media as Media[])
     : [];
 
   return (
